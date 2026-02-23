d0efc203af07c5ef98f8ec08131f502ea0417ead
Rajiv Khanal <rajivkhanal781@gmail.com>
2026-02-23 23:15:28 +0545

    defaults defined

---
+++ password-manager.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <fstream> // this header is to provide classes for file input and output
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+#define WEBSITE 100
+#define USERNAME 18
+#define PASSWORD 27
+#define TABLE 50
+
